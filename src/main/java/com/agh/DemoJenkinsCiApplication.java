bd168c74f0b8250aa4020465ba04f40e5e5215b3
aghilas <aghilas.djema@ext.leroymerlin.fr>
2019-11-17 14:34:43 +0100

    init project

---
+++ src/main/java/com/agh/DemoJenkinsCiApplication.java
@@ -0,0 +1,13 @@
+package com.agh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinsCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agh/DemoJenkinsCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
